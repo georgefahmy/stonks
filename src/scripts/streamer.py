266c9d4ab67e70736886502cb9f19124a5da0d7b
George Fahmy <george@kespry.com>
2020-03-18 16:41:57 -0700

    update readme for correct nltk_download path

--- src/scripts/streamer.py
+++ src/scripts/streamer.py
@@ -61,8 +61,8 @@ def get_arg_parser():
         "-p",
         "--no-price",
         action="store_true",
-        default=True,
-        help="optional flag to display pricing info. Default=False",
+        default=False,
+        help="optional flag to hide pricing info. Default=False",
     )
 
     arg_parser.add_argument(
@@ -187,7 +187,7 @@ def main(*args):
     if parsed.sentiment:
         logger.info("Sentiment flag set: comment sentiment will be interpreted (experimental)")
 
-    if not parsed.price:
+    if parsed.no_price:
         logger.info("no-price flag is set. price information will be turned off.")
 
     logger.info("Starting stream!")
@@ -212,10 +212,10 @@ def main(*args):
                 )
                 print("Stocks Found:")
                 for ticker in list(set(ticker_list)):
-                    if parsed.price:
+                    if parsed.no_price:
                         price_string = ""
 
-                    elif not parsed.price:
+                    elif not parsed.no_price:
                         ticker_price = round(si.get_live_price(ticker), 3)
                         ticker_prct = round(
                             (
